a59f9bbefae1f1eef75d1775bd5d2065206e091c
Kory <korygm33@gmail.com>
2022-03-29 02:33:22 +0900

    chore: reformat

--- src/main/java/me/vik1395/bungeekick/PlayerListener.java
+++ src/main/java/me/vik1395/bungeekick/PlayerListener.java
@@ -11,22 +11,20 @@ import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
 /*
-
-Author: Vik1395
-Project: BungeeKick
-
-Copyright 2014
-
-Licensed under Creative CommonsAttribution-ShareAlike 4.0 International Public License (the "License");
-You may not use this file except in compliance with the License.
-
-You may obtain a copy of the License at http://creativecommons.org/licenses/by-sa/4.0/legalcode
-
-You may find an abridged version of the License at http://creativecommons.org/licenses/by-sa/4.0/
+ * Author: Vik1395
+ * Project: BungeeKick
+ * 
+ * Copyright 2014
+ * 
+ * Licensed under Creative CommonsAttribution-ShareAlike 4.0 International Public License (the "License");
+ * You may not use this file except in compliance with the License.
+ * 
+ * You may obtain a copy of the License at http://creativecommons.org/licenses/by-sa/4.0/legalcode
+ * 
+ * You may find an abridged version of the License at http://creativecommons.org/licenses/by-sa/4.0/
  */
 
 public class PlayerListener implements Listener {
-
     BungeeKick plugin;
 
     public PlayerListener(BungeeKick plugin) {
@@ -34,44 +32,39 @@ public class PlayerListener implements Listener {
     }
 
     @EventHandler
-    public void onServerKickEvent(ServerKickEvent ev) 
-    {
+    public void onServerKickEvent(ServerKickEvent ev) {
         ServerInfo kickedFrom = null;
 
-        if (ev.getPlayer().getServer() != null) 
-        {
+        if (ev.getPlayer().getServer() != null) {
             kickedFrom = ev.getPlayer().getServer().getInfo();
-        } 
-        
-        else if (this.plugin.getProxy().getReconnectHandler() != null) 
-        {
+        }
+
+        else if (this.plugin.getProxy().getReconnectHandler() != null) {
             kickedFrom = this.plugin.getProxy().getReconnectHandler().getServer(ev.getPlayer());
-        } 
-        
-        else 
-        {
+        }
+
+        else {
             kickedFrom = AbstractReconnectHandler.getForcedHost(ev.getPlayer().getPendingConnection());
-            if (kickedFrom == null)
-            {
-                kickedFrom = ProxyServer.getInstance().getServerInfo(ev.getPlayer().getPendingConnection().getListener().getDefaultServer());
+            if (kickedFrom == null) {
+                kickedFrom = ProxyServer.getInstance()
+                        .getServerInfo(ev.getPlayer().getPendingConnection().getListener().getDefaultServer());
             }
         }
 
         ServerInfo kickTo = this.plugin.getProxy().getServerInfo(BungeeKick.config.getString("ServerName"));
-        
+
         if (kickedFrom != null && kickedFrom.equals(kickTo)) {
             return;
         }
-        
+
         ev.setCancelled(true);
         ev.setCancelServer(kickTo);
-        if(BungeeKick.config.getBoolean("ShowKickMessage"))
-        {
-	        String msg = BungeeKick.config.getString("KickMessage");
-	        msg = ChatColor.translateAlternateColorCodes('&', msg);
-	        String kmsg = ChatColor.stripColor(BaseComponent.toLegacyText(ev.getKickReasonComponent()));
-	        msg = msg + kmsg;
-	        ev.getPlayer().sendMessage(new TextComponent(msg));
+        if (BungeeKick.config.getBoolean("ShowKickMessage")) {
+            String msg = BungeeKick.config.getString("KickMessage");
+            msg = ChatColor.translateAlternateColorCodes('&', msg);
+            String kmsg = ChatColor.stripColor(BaseComponent.toLegacyText(ev.getKickReasonComponent()));
+            msg = msg + kmsg;
+            ev.getPlayer().sendMessage(new TextComponent(msg));
         }
     }
 }
